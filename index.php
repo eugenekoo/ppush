62a7421cdf24af3749b7b6126b4f2911101c2267
eugenekoo <koolah@hotmail.com>
2015-06-10 00:59:01 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 include_once 'class.push.php';
-$push	= new pushmessage();
+//$push	= new pushmessage();
 
-$params	= array("pushtype"=>"android", $idphone=>"android_smart_phone_id_here", $mst=>"Hello, an android user");
-$rtn = $push->sendMessage($params);
+//$params	= array("pushtype"=>"android", $idphone=>"android_smart_phone_id_here", $mst=>"Hello, an android user");
+//$rtn = $push->sendMessage($params);
+echo "apa ni?";
 //print_r($rtn);
